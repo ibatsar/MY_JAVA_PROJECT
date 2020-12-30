5afbc8e0d210d8c1bea7769bb647cb205d61d446
User <User@DESKTOP-0P79U3O>
2020-12-30 20:31:04 +0900

    chapter04

---
+++ src/icehs/science/chapter04/CalculatorTest.java
@@ -0,0 +1,24 @@
+package icehs.science.chapter04;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		int number1 = 20;
+		int number2 = 7;
+		
+		int plusResult = number1 + number2;
+		int minusResult = number1 - number2;
+		int multiplication = number1 * number2;
+		int divisionResult = number1 / number2;
+		int remainderResult = number1 % number2;
+		System.out.println("µ¡¼À°á°ú : " + plusResult);
+		System.out.println("»¬¼À°á°ú : " + minusResult);
+		System.out.println("°ö¼À°á°ú : " + multiplication);
+		System.out.println("³ª´°¼À°á°ú : " + divisionResult);
+		System.out.println("³ª¸ÓÁö : " + remainderResult);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
