3bae8b6f194e3f6d213e9751d9a117752f125df0
User <User@DESKTOP-0P79U3O>
2021-01-04 21:10:03 +0900

    chapter06

---
+++ src/icehs/science/chapter06/CoffeeTest.java
@@ -0,0 +1,19 @@
+package icehs.science.chapter06;
+
+public class CoffeeTest {
+	public static void main(String[] args) {
+		Coffee ame = new Coffee();
+		Coffee latte = new Coffee();
+		Coffee maki = new Coffee();
+		ame.name = "아메리카노";
+		ame.price = 3500;
+		ame.printCoffeeInfo();
+		
+		latte.name = "카페라떼";
+		latte.price = 4000;
+		latte.printCoffeeInfo();
+		
+		int priceSum = ame.getPrice() + latte.getPrice() + maki.getPrice();
+		System.out.println("합계 : " + priceSum + "원");
+
+}
